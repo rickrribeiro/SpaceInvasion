7c21011bed100549d17479e141883d414ecaa998
ricardobah <rickribeiro99@gmail.com>
2020-02-09 00:03:41 +0000

    classes and images

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,23 +1,27 @@
 import sys
 import pygame
 from settings import Settings
-from ship import ship
-
+from ship import Ship
+import game_functions as gf 
 def run_game():
         # Initialize game and create a screen object.u     
         pygame.init()   
         ai_settings = Settings()  
         screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
         pygame.display.set_caption("Alien Invasion") 
+        ship = Ship(ai_settings,screen)
         bg_color = (230, 230, 230)   
         # Start the main loop for the game.w     
         while True:
                     # Watch for keyboard and mouse events.x         
-            for event in pygame.event.get():   
-                if event.type == pygame.QUIT:                
-                    sys.exit()        # Make the most recently drawn screen visible.z
+            gf.check_events(ship)
+            ship.update()
             # Redraw the screen during each pass through the loop.
-            screen.fill(ai_settings.bg_color)
-            pygame.display.flip()
+            gf.update_screen(ai_settings,screen,ship)
 
 run_game()
+
+
+
+
+#Refactoring is a disciplined technique for restructuring an existing body of code, altering its internal structure without changing its external behavior. Its heart is a series of small behavior preserving transformations.
--- ship.py
+++ ship.py
@@ -1,18 +1,31 @@
 import pygame
 
-class ship():
-    def __init__(self,screen): #self reference and the screen where the shil will be drawed
+class Ship():
+    def __init__(self,ai_settings,screen): #self reference and the screen where the shil will be drawed
         #initialize and set position
-        self.screen()=screen
+        self.screen =screen
+        self.ai_settings = ai_settings
         #load image
         self.image = pygame.image.load('assets/ship.bmp')
-        self.rect = self.image.get_rect() #Python 
+        self.rect = self.image.get_rect() #Python let you to treat the elements as rectangles
         self.screen_rect = screen.get_rect()
 
-        #start each new ship at the botton center
+        #start each new ship at the bottom center
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.botton = self.screen_rect.botton
+        self.rect.bottom = self.screen_rect.bottom
+        self.moving_right = False
+        self.moving_left = False
 
+        #store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
+        
     def blitme(self):
         #draw the ship at its current location
         self.screen.blit(self.image, self.rect)
